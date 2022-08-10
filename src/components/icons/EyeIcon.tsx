ba52939e4caa756b0f7aaef43844ad9674b51a5f
Gabriel <gabrielm.linassi@gmail.com>
2022-08-10 15:08:46 -0300

    fix lint error

--- src/components/icons/EyeIcon.tsx
+++ src/components/icons/EyeIcon.tsx
@@ -19,7 +19,7 @@ const EyeIcon = ({ className, ...props }: SVGProps<SVGSVGElement>) => {
       />
       <path
         strokeLinecap="round"
-        stroke-strokeLinejoin="round"
+        strokeLinejoin="round"
         d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
       />
     </svg>
