a4dfcf18af4c0b1c8d983d513ff45ca5784f408b
Gabriel <gabrielm.linassi@gmail.com>
2022-08-10 10:47:57 -0300

    Add ButtonIcon comp

---
+++ src/components/button-icon/ButtonIcon.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Slot } from '@radix-ui/react-slot'
+
+type AsButton = {
+  asChild?: false
+} & React.ComponentPropsWithoutRef<'button'>
+
+type AsSlot = {
+  asChild?: true
+}
+
+type ButttonIconProps = {
+  children: React.ReactNode
+} & (AsButton | AsSlot)
+
+const ButtonIcon = ({ children, asChild, ...props }: ButttonIconProps) => {
+  const Comp = asChild ? Slot : 'button'
+  return (
+    <Comp
+      className="flex h-12 w-12 items-center justify-center rounded-lg border border-[#4A5465] bg-[#252932]"
+      {...props}
+    >
+      {children}
+    </Comp>
+  )
+}
+
+export default ButtonIcon
