fc209d874c92bcd8dc838d789b9ad1c285c2a361
Gabriel <gabrielm.linassi@gmail.com>
2022-08-10 15:10:09 -0300

    remove test Comp & updt home page

--- src/components/test/Test.tsx
+++
@@ -1,8 +0,0 @@
-import s from './Test.module.css';
-import { cnMerge } from 'utils/cn-merge';
-
-function Test() {
-  return <h1 className={cnMerge('font-bold text-green-500', s['something'])}>Test</h1>;
-}
-
-export default Test;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,50 @@
-import type { NextPage } from 'next'
-import Test from 'components/test'
+import type { NextPage } from 'next';
+import NextLink from 'next/link';
+import ButtonIcon from 'components/button-icon';
+import { AppleIcon, FbIcon, GoogleIcon } from 'components/icons';
+import TextField from 'components/text-field/TextField';
 
 const HomePage: NextPage = () => {
   return (
-    <div className="bg-yellow-500 text-red-500">
-      Home Page
-      <Test />
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-600">
+      <div className="flex h-80 w-full max-w-sm flex-col items-center justify-end rounded-md bg-gray-800 p-4">
+        <div className="mt-4 flex gap-4">
+          {/* link (route) */}
+          <NextLink href="/account" passHref>
+            <ButtonIcon asChild>
+              <a>
+                <AppleIcon />
+              </a>
+            </ButtonIcon>
+          </NextLink>
+          {/* external link */}
+          <ButtonIcon asChild>
+            <a
+              href="https://www.linkedin.com/in/gabrielmlinassi/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FbIcon />
+            </a>
+          </ButtonIcon>
+          {/* button */}
+          <ButtonIcon onClick={() => alert('clicked')}>
+            <GoogleIcon />
+          </ButtonIcon>
+        </div>
+        <div className="mt-4 w-full space-y-4">
+          <TextField name="fullName" label="Full Name" type="text" fullWidth />
+          <TextField
+            name="email"
+            label="Email"
+            type="email"
+            placeholder="ie. john.doe@email.com"
+          />
+          <TextField fullWidth name="pwd" label="Password" type="password" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
