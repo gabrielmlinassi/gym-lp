af760e4c72c9b28f76b9bc8c9f24371e69af7a27
Gabriel <gabrielm.linassi@gmail.com>
2022-08-10 16:39:57 -0300

    updt tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    keyframes: {
+      // Dialog
+      'fade-in': {
+        '0%': { opacity: 0 },
+        '100%': { opacity: 1 },
+      },
+      'fade-out': {
+        '0%': { opacity: 1 },
+        '100%': { opacity: 0 },
+      },
+    },
+    animation: {
+      // Dialog
+      'fade-in': 'fade-in 0.2s ease',
+      'fade-out': 'fade-out 0.2s ease',
+    },
   },
   plugins: [
     require('tailwindcss-radix')({
