9ba05cf38e6b24cc1dfd7b9d6c85b6f87440d044
Gabriel <gabrielm.linassi@gmail.com>
2022-08-10 16:39:33 -0300

    Add SignIn page

---
+++ src/pages/signin.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const SignInPage: NextPage = () => {
+  return <div className="bg-yellow-500 text-red-500">SignIn Page</div>;
+};
+
+export default SignInPage;
