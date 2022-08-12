75ffeadf5efa759232b1c5ccedfee590f15e9c3b
Gabriel <gabrielm.linassi@gmail.com>
2022-08-12 11:18:54 -0300

    Fix props

--- src/components/icons/XIcon.tsx
+++ src/components/icons/XIcon.tsx
@@ -5,8 +5,6 @@ const XIcon = ({ className, ...props }: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="14"
-      height="14"
       viewBox="0 0 14 14"
       fill="none"
       className={twMerge('h-6 w-6 text-[#FAA806]', className)}
