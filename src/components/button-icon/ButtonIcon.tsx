4b3cf4d122c7e2e6ce5b16ef68ef99b848771c54
Gabriel <gabrielm.linassi@gmail.com>
2022-08-12 11:18:35 -0300

    Replace CSS @apply by object

--- src/components/button-icon/ButtonIcon.tsx
+++ src/components/button-icon/ButtonIcon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Slot } from '@radix-ui/react-slot';
+import { cnMerge } from 'utils/cn-merge';
 
 type AsButton = {
   asChild?: false;
@@ -11,15 +12,29 @@ type AsSlot = {
 
 type ButttonIconProps = {
   children: React.ReactNode;
+  variant?: 'filled' | 'subtle';
+  size?: 'base' | 'sm';
 } & (AsButton | AsSlot);
 
+const classes = {
+  root: /*tw:*/ `flex items-center justify-center p-1.5 rounded-lg`,
+  sizes: {
+    base: /*tw:*/ `h-12 w-12`,
+    sm: /*tw:*/ `h-7 w-7`,
+  },
+  variants: {
+    filled: /*tw:*/ `bg-[#252932] border border-[#4A5465]`,
+    subtle: /*tw:*/ `hover:bg-black hover:bg-opacity-10`,
+  },
+};
+
 const ButtonIcon = React.forwardRef<any, ButttonIconProps>(
-  ({ children, asChild, ...props }, forwardedRef) => {
+  ({ children, variant = 'filled', size = 'base', asChild, ...props }, forwardedRef) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
         ref={forwardedRef}
-        className="flex h-12 w-12 items-center justify-center rounded-lg border border-[#4A5465] bg-[#252932]"
+        className={cnMerge(classes.root, classes.variants[variant], classes.sizes[size])}
         {...props}
       >
         {children}
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,6 +1,5 @@
-import s from './Button.module.css';
 import React from 'react';
-import cn from 'classnames';
+import { cnMerge } from 'utils/cn-merge';
 import Spinner from 'components/spinner';
 
 type ButtonProps = {
@@ -10,7 +9,26 @@ type ButtonProps = {
   variant?: 'filled' | 'outlined';
   color?: 'primary' | 'secondary';
   size?: 'base' | 'lg';
-} & Omit<React.ComponentProps<'button'>, 'className'>;
+} & React.ComponentProps<'button'>;
+
+const s = {
+  root: /*tw:*/ `inline-flex items-center justify-center rounded-full font-semibold duration-150 disabled:pointer-events-none disabled:opacity-75`,
+  fullWidth: /*tw:*/ `w-full`,
+  variants: {
+    'filled.primary': /*tw:*/ `bg-[#FAA806] text-[#FFFFFF] hover:bg-[#EE9F04]`,
+    'filled.secondary': /*tw:*/ `bg-[#373E4B] text-[#97A3B7] hover:bg-[#343A47]`,
+    'outlined.primary': /*tw:*/ `border-[#FAA806] text-[#FAA806]`,
+    'outlined.secondary': /*tw:*/ `border-[#373E4B] text-[#373E4B]`,
+  },
+  sizes: {
+    base: /*tw:*/ `px-8 py-3`,
+    lg: /*tw:*/ `px-12 py-5`,
+  },
+  spinner: {
+    primary: /*tw:*/ `fill-[#bc7e03] text-white`,
+    secondary: /*tw:*/ `fill-[#292e38] text-white`,
+  },
+};
 
 const Button = React.forwardRef<any, ButtonProps>(
   (
@@ -22,13 +40,18 @@ const Button = React.forwardRef<any, ButtonProps>(
       fullWidth,
       loading,
       disabled,
+      className,
       ...props
     },
     forwardedRef
   ) => {
-    const classes = cn(s.root, s[variant], s[color], s[size], {
-      [s.fullWidth]: fullWidth,
-    });
+    const classes = cnMerge(
+      s.root,
+      s.variants[`${variant}.${color}`],
+      s.sizes[size],
+      { [s.fullWidth]: fullWidth },
+      className
+    );
 
     return (
       <button
@@ -40,7 +63,7 @@ const Button = React.forwardRef<any, ButtonProps>(
         {children}
         {loading && (
           <span className="ml-1.5">
-            <Spinner className={s.spinner} />
+            <Spinner className={s.spinner[color]} />
           </span>
         )}
       </button>
