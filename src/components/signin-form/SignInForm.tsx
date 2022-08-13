9952c6f7378da1bb807a70d6edce96f8d071000c
Gabriel <gabrielm.linassi@gmail.com>
2022-08-13 10:35:23 -0300

    Improvements to SignInForm

--- src/components/signin-form/SignInForm.tsx
+++ src/components/signin-form/SignInForm.tsx
@@ -1,5 +1,45 @@
+import NextLink from 'next/link';
+
+import Button from 'components/button';
+import TextField from 'components/text-field';
+import Checkbox from 'components/checkbox';
+
 const SignInForm = () => {
-  return <h1>SignIn Form</h1>;
+  return (
+    <form className="mt-6 space-y-4">
+      <TextField name="fullName" label="Full Name" type="text" fullWidth />
+      <TextField
+        name="email"
+        label="Email"
+        type="email"
+        autoComplete="username"
+        placeholder="ie. john.doe@email.com"
+        fullWidth
+      />
+      <TextField
+        name="pwd"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        fullWidth
+      />
+      <Checkbox
+        id="terms"
+        name="terms"
+        label={
+          <>
+            I confirm I&apos;ve read and accept{' '}
+            <NextLink href="/terms">
+              <a className="text-[#FAA806]">terms &amp; conditions</a>
+            </NextLink>
+          </>
+        }
+      />
+      <Button color="secondary" disabled fullWidth>
+        Register
+      </Button>
+    </form>
+  );
 };
 
 export default SignInForm;
