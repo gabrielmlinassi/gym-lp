b46623b728272b3060ab0ffc65f158a80443ee48
Gabriel <gabrielm.linassi@gmail.com>
2022-08-13 10:35:34 -0300

    Improvements to SignInDialog

--- src/components/signin-dialog/SignInDialog.tsx
+++ src/components/signin-dialog/SignInDialog.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import NextLink from 'next/link';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
+
+import { XIcon } from 'components/icons';
+import ButtonIcon from 'components/button-icon';
+import { AppleIcon, FbIcon, GoogleIcon } from 'components/icons';
+import SigninForm from 'components/signin-form';
 
 type DialogProps = {} & React.ComponentProps<typeof DialogPrimitive.Root>;
 
 const classes = {
-  overlay: /*tw:*/ `absolute inset-0 bg-black bg-opacity-25 rdx-state-open:animate-fade-in rdx-state-closed:animate-fade-out`,
-  content: /*tw:*/ `absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[600px] bg-white p-4 rounded-lg rdx-state-open:animate-fade-in rdx-state-closed:animate-fade-out`,
+  overlay: /*tw:*/ `fixed inset-0 bg-black bg-opacity-25 rdx-state-open:animate-fade-in rdx-state-closed:animate-fade-out`,
+  content: /*tw:*/ `fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] bg-[#1E2229] p-12 rounded-3xl rdx-state-open:animate-fade-in rdx-state-closed:animate-fade-out`,
 };
 
 const SignInDialog = ({ children, ...props }: DialogProps) => {
@@ -15,11 +21,43 @@ const SignInDialog = ({ children, ...props }: DialogProps) => {
       <DialogPrimitive.Portal>
         <DialogPrimitive.Overlay className={classes.overlay} />
         <DialogPrimitive.Content className={classes.content}>
-          <div className="flex items-center justify-between">
-            <DialogPrimitive.Title>Title</DialogPrimitive.Title>
-            <DialogPrimitive.Close>Close</DialogPrimitive.Close>
+          <div className="relative">
+            <DialogPrimitive.Title className="text-center font-industry text-[22px] font-semibold uppercase text-white">
+              Register with email
+            </DialogPrimitive.Title>
+            <div className="absolute top-0 right-0 translate-x-full">
+              <DialogPrimitive.Close asChild>
+                <ButtonIcon variant="subtle" size="sm">
+                  <XIcon className="h-full w-full" />
+                </ButtonIcon>
+              </DialogPrimitive.Close>
+            </div>
+            <SigninForm />
+            <div className="mt-6">
+              <div className="flex items-center gap-2">
+                <hr className="w-full border-[#4A5465]" />
+                <span className="font-industry text-white">OR</span>
+                <hr className="w-full border-[#4A5465]" />
+              </div>
+              <div className="mt-6 flex items-center justify-center gap-4">
+                <NextLink href="/account" passHref>
+                  <ButtonIcon asChild>
+                    <a>
+                      <AppleIcon />
+                    </a>
+                  </ButtonIcon>
+                </NextLink>
+                <ButtonIcon asChild>
+                  <a>
+                    <FbIcon />
+                  </a>
+                </ButtonIcon>
+                <ButtonIcon>
+                  <GoogleIcon />
+                </ButtonIcon>
+              </div>
+            </div>
           </div>
-          <DialogPrimitive.Description>Description</DialogPrimitive.Description>
         </DialogPrimitive.Content>
       </DialogPrimitive.Portal>
     </DialogPrimitive.Root>
