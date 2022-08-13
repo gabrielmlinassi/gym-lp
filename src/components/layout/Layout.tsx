a8749ddf935c1f8b0ec9beeff48d18f7d1ab8f80
Gabriel <gabrielm.linassi@gmail.com>
2022-08-13 10:35:55 -0300

    Improvements to Layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,21 @@
-const Layout = () => {
-  return <h1>layout</h1>;
+import React from 'react';
+import Container from 'components/container';
+import Navbar from 'components/navbar';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <Container variant="outer">
+      <Navbar />
+      <main className="pt-28 md:pt-36">{children}</main>
+    </Container>
+  );
 };
 
 export default Layout;
+
+// <-- 1120px -->
+//mx-auto max-w-[calc(1120px+32px)] px-4
