e67d341955dce5758505ef94a6e32216692fe497
Gabriel <gabrielm.linassi@gmail.com>
2022-08-13 22:17:30 -0300

    Fix style Checkbox Comp

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -6,13 +6,13 @@ type CheckboxProps = {
 } & Omit<React.ComponentProps<'input'>, 'type'>;
 
 const s = {
-  input: /*tw:*/ `h-6 w-6 rounded-lg border-[#4A5465] bg-[#252932] text-[#FAA806] checked:bg-[#FAA806] hover:ring-0`,
+  input: /*tw:*/ `h-6 w-6 rounded-lg border-[#4A5465] mt-0.5 bg-[#252932] text-[#FAA806] checked:bg-[#FAA806] hover:ring-0`,
   label: /*tw:*/ `select-none font-proximaNova font-normal text-[#F8F8F9]`,
 };
 
 const Checkbox = ({ label, className, ...props }: CheckboxProps) => {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex gap-2">
       <input type="checkbox" className={cnMerge(s.input, className)} {...props} />
       <label htmlFor="terms" className={s.label}>
         {label}
