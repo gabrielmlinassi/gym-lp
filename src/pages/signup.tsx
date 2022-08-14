5e9cf2f73f70f63c8135972337ba6d8c7591307c
Gabriel <gabrielm.linassi@gmail.com>
2022-08-13 22:20:20 -0300

    Add SignIn and Up pages

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -8,13 +8,13 @@ const SignInPage: NextPage = () => {
   return (
     <div className="mt-12">
       <div className="text-center">
-        <Heading variant="h1">Log In</Heading>
+        <Heading variant="h1">Register</Heading>
       </div>
       <div className="mx-auto mt-6 max-w-[500px] rounded-3xl bg-[#1E2229] p-12">
         <SigninForm
           actions={
             <Button type="submit" color="primary" disabled={false} fullWidth>
-              Log In
+              Register
             </Button>
           }
         />
