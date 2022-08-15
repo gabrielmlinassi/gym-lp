92fcb1a3fd9b4fb3c78c986c385b29b7207a3266
Gabriel <gabrielm.linassi@gmail.com>
2022-08-14 22:18:53 -0300

    Separate Reviews Comp into Reviews & SuccessStories

---
+++ src/components/sections/success-stories/SuccessStories.tsx
@@ -0,0 +1,41 @@
+import NextImage from 'next/image';
+
+import Container from 'components/container';
+import { Heading, Text } from 'components/typography';
+
+import logoNcaa from '/public/logos/logo-ncaa.svg';
+import logoNfl from '/public/logos/logo-nfl.svg';
+import logoMll from '/public/logos/logo-mll.svg';
+import logoOlympics from '/public/logos/logo-olympics.svg';
+import logoDiamondLeague from '/public/logos/logo-diamond-league.svg';
+
+const SuccessStories = () => {
+  return (
+    <>
+      <Container variant="inner">
+        <div className="mt-32 text-center">
+          <Heading variant="h2">Proven results</Heading>
+        </div>
+        <div className="mx-auto max-w-[575px] text-center">
+          <Text>
+            Join the training system that has produced countless Division I athletes,
+            All-Americans, State Champions, and Olympians!
+          </Text>
+        </div>
+      </Container>
+      <div className="mt-20 bg-[#373E4B] py-14">
+        <Container variant="inner" className="h-full">
+          <div className="flex h-full flex-wrap items-center justify-center gap-8 sm:flex-nowrap md:gap-16">
+            <NextImage src={logoNcaa} />
+            <NextImage src={logoNfl} />
+            <NextImage src={logoMll} />
+            <NextImage src={logoOlympics} />
+            <NextImage src={logoDiamondLeague} />
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default SuccessStories;
