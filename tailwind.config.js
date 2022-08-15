f4715527e275ff3c1c9837a8e98845c94b2c3f24
Gabriel <gabrielm.linassi@gmail.com>
2022-08-14 22:35:55 -0300

    Fix tailwind.config give preference to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,22 +9,22 @@ module.exports = {
         industry: ['Industry', ...defaultTheme.fontFamily.sans],
         proximaNova: ['Proxima Nova', ...defaultTheme.fontFamily.sans],
       },
-    },
-    keyframes: {
-      // Dialog
-      'fade-in': {
-        '0%': { opacity: 0 },
-        '100%': { opacity: 1 },
+      keyframes: {
+        // Dialog
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        'fade-out': {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
       },
-      'fade-out': {
-        '0%': { opacity: 1 },
-        '100%': { opacity: 0 },
+      animation: {
+        // Dialog
+        'fade-in': 'fade-in 0.2s ease',
+        'fade-out': 'fade-out 0.2s ease',
       },
-    },
-    animation: {
-      // Dialog
-      'fade-in': 'fade-in 0.2s ease',
-      'fade-out': 'fade-out 0.2s ease',
     },
   },
   plugins: [
