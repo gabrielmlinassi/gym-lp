d88a78719ad3a12a8e6f0af5b54be0bae5a01b9f
Gabriel <gabrielm.linassi@gmail.com>
2022-08-15 23:19:09 -0300

    Add remaining comps to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,46 @@
+import { Suspense } from 'react';
 import type { NextPage } from 'next';
-import NextLink from 'next/link';
-import ButtonIcon from 'components/button-icon';
-import { AppleIcon, FbIcon, GoogleIcon } from 'components/icons';
-import TextField from 'components/text-field/TextField';
+
+import Hero from '@sections/hero';
+import WhatFor from '@sections/what-for';
+import HowItWorks from '@sections/how-it-works';
+import Reviews from '@sections/reviews';
+import Screenshots from '@sections/screenshots';
+import SuccessStories from '@sections/success-stories';
 
 const HomePage: NextPage = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-600">
-      <div className="flex h-80 w-full max-w-sm flex-col items-center justify-end rounded-md bg-gray-800 p-4">
-        <div className="mt-4 flex gap-4">
-          {/* link (route) */}
-          <NextLink href="/account" passHref>
-            <ButtonIcon asChild>
-              <a>
-                <AppleIcon />
-              </a>
-            </ButtonIcon>
-          </NextLink>
-          {/* external link */}
-          <ButtonIcon asChild>
-            <a
-              href="https://www.linkedin.com/in/gabrielmlinassi/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FbIcon />
-            </a>
-          </ButtonIcon>
-          {/* button */}
-          <ButtonIcon onClick={() => alert('clicked')}>
-            <GoogleIcon />
-          </ButtonIcon>
+    <div className="pt-8">
+      <Hero />
+      <Suspense>
+        <WhatFor />
+        <HowItWorks />
+        <Screenshots />
+        <SuccessStories />
+        <Reviews />
+      </Suspense>
+      {/* <div className="h-[780px] bg-yellow-600">
+        <Logo variant="full" />
+        <div className="max-w-[450px] border-2 border-dashed">
+          <Heading variant="h1">Strength Training Built For You</Heading>
         </div>
-        <div className="mt-4 w-full space-y-4">
-          <TextField name="fullName" label="Full Name" type="text" fullWidth />
-          <TextField
-            name="email"
-            label="Email"
-            type="email"
-            placeholder="ie. john.doe@email.com"
-          />
-          <TextField fullWidth name="pwd" label="Password" type="password" />
+      </div> */}
+
+      {/* <div className="flex h-screen w-screen items-center justify-center">
+        <div className="flex h-80 w-full max-w-sm flex-col items-center justify-end rounded-md bg-gray-800 p-4">
+          <NextImage src={AppStoreImg} />
+          <SignInDialog onOpenChange={(open) => router.push({}, open ? '/signin' : '')}>
+            <Button>Open</Button>
+          </SignInDialog>
         </div>
-      </div>
+      </div> */}
+      {/* <div className="relative h-[668px]">
+        <NextImage
+          src={bgAthlete}
+          layout="fill"
+          className="object-cover object-[55%,50%]"
+        />
+      </div> */}
     </div>
   );
 };
